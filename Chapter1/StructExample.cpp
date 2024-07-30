20c8fdb556da3a7dba6e393f73abd5d4f5a66c50
lt47 <layetee@gmail.com>
2024-07-29 19:40:42 -0500

    completed reinforcement 1.3.

---
+++ Chapter1/StructExample.cpp
@@ -0,0 +1,9 @@
+/** A structure is used for gathering an aggregation of elements. These elements can be different, their grouping is not based on their similarities.
+ * R-1.3)
+ * It provides references to data only. No methods. It is a data-only, less flexible version of a class. Native to C. 
+*/
+
+struct Pair{
+    int first;
+    double second;
+};
