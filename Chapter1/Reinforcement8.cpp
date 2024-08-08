f8da55f83f13990e766420b9333d31b641e3ede9
lt47 <layetee@gmail.com>
2024-08-07 23:13:41 -0500

    Completed reinforcement 8.

--- Chapter1/Reinforcement8.cpp
+++ Chapter1/Reinforcement8.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
-
-bool isMultiple(long &x, long &y){
-
+/**Reinforcement 8*/
+bool isMultiple(long& n, long& m){
+    int i;
+    
+    /*The for loop is only necessary when i needs to be determine. Otherwise, the modulus operator could have been used to determine if n is a valid multiple. */ 
+    for(i=0; i<n; i++){
+        if(n == m*i){
+            std::cout << true << std::endl;
+            return true;
+        }
+    }
+    std::cout << false << std::endl;
     return false;
+}
+int main(){
+    long int exampleMultiple = 30;
+    long int example = 5;
+    isMultiple(exampleMultiple, example);
+    
 }
