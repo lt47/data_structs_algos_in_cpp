2717da8e61ef936aa2746f86292a32e710e0d246
lt47 <layetee@gmail.com>
2024-09-02 10:40:42 -0500

    completed reinforcement 15

--- Chapter1/CreditCard.h
+++ Chapter1/CreditCard.h
@@ -9,12 +9,19 @@ class CreditCard{
     public:
         CreditCard(const std::string& no, const std::string& nm, int lim, double bal=0);
         std::string getNumber() const {return number;}
+        void setNumber(const std::string& no){number = no;}
         std::string getName() const {return name;}
-        /* RE: Reinforcement 13 (notes on Line 16 were written prior). Lets assume a credit card interest rate of 5.99%*/
-        double getBalance() const {return balance * 0.0599;}
+        void setName(const std::string& nm){name = nm;}
+        /* RE: Reinforcement 13. Lets assume a credit card interest rate of 5.99%*/
+        double getBalance() const {return balance * interestRate;}
+        void setBalance(double bal){balance = bal;}
         int getLimit() const {return limit;}
+        void setLimit(int lim){limit = lim;}
+        double getLateFee() const {return lateFee;}
+        void setLateFee(double fee){lateFee = fee;}
+        
         std::time_t getDueDate(); //The method will be defined in CreditCard.cpp 
-        double getLateFee() const {return 29.00;}
+
 
         /*For Reinforcement 13, my initial instinct is that the interest should be tacked on to the chargeIt function, but it says to charge interest to each payment. 
         * I checked the hint and it says interest should be tacked on to the existing balance, which makes sense. 
@@ -28,6 +35,8 @@ class CreditCard{
         int limit;
         double balance;
         time_t dueDate;
+        double interestRate = 0.0599;
+        double lateFee = 29.00;
 };
 
 // Print card information. 
