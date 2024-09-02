09b76ec0cc83b6288e3aca2d7d6b7ff7e3590ed3
lt47 <layetee@gmail.com>
2024-09-02 08:38:39 -0500

    added code for reinforcement 14

---
+++ Chapter1/CreditCard.cpp
@@ -0,0 +1,61 @@
+#include "CreditCard.h"
+
+using namespace std;
+
+CreditCard::CreditCard(const string& no, const string& nm, int lim, double bal){
+    number = no;
+    name = nm;
+    balance = bal;
+    limit = lim;
+}
+
+time_t CreditCard::getDueDate(){
+    // Get the current time
+    time_t now = time(NULL);
+
+    // Convert time_t to struct tm
+    struct tm *currentDueDate = localtime(&now);
+
+    // Set the day of the month to 1 (first day). Using the '->' notation because it is a pointer and thats how pointer attributes are modified/accessed.
+    currentDueDate->tm_mday = 1;
+
+    // Set the time to midnight (00:00:00)
+    currentDueDate->tm_hour = 0;
+    currentDueDate->tm_min = 0;
+    currentDueDate->tm_sec = 0;
+
+    // Convert struct tm back to time_t
+    dueDate = mktime(currentDueDate);
+    return dueDate;
+}
+
+bool CreditCard::chargeIt(double price){
+    if (price < 0.0){return false;}
+    else if (price + balance > double(limit)){ return false;}
+    balance += price;
+    return true;
+}
+
+void CreditCard::makePayment(double payment){
+    // Get the timestamp for the current date and time
+    time_t currentDateTime;
+    time(&currentDateTime);
+    // this ensures that the payment is always positive. No action is taken otherwise.
+    if (payment < 0.0){return;}
+    // Check if the payment was made after the due date.
+    else if (currentDateTime > getDueDate()){
+        balance -= payment;
+        // Charge late payment fee per Reinforcement 14. 
+        balance += getLateFee();
+    }
+    balance -= payment;
+}
+
+ostream& operator<<(ostream& out, const CreditCard& c){
+    out << "Number = " << c.getNumber() << "\n"
+        << "Name = " << c.getName() << "\n"
+        << "Balance = " << c.getBalance() << "\n"
+        << "Limit = " << c.getLimit() << "\n";
+    
+    return out;
+}
--- Chapter1/CreditCard.h
+++ Chapter1/CreditCard.h
@@ -3,15 +3,22 @@
 
 #include <string>
 #include <iostream>
+#include <ctime>
 
 class CreditCard{
     public:
         CreditCard(const std::string& no, const std::string& nm, int lim, double bal=0);
         std::string getNumber() const {return number;}
         std::string getName() const {return name;}
-        double getBalance() const {return balance;}
+        /* RE: Reinforcement 13 (notes on Line 16 were written prior). Lets assume a credit card interest rate of 5.99%*/
+        double getBalance() const {return balance * 0.0599;}
         int getLimit() const {return limit;}
+        std::time_t getDueDate(); //The method will be defined in CreditCard.cpp 
+        double getLateFee() const {return 29.00;}
 
+        /*For Reinforcement 13, my initial instinct is that the interest should be tacked on to the chargeIt function, but it says to charge interest to each payment. 
+        * I checked the hint and it says interest should be tacked on to the existing balance, which makes sense. 
+        */
         bool chargeIt(double price);
         void makePayment(double payment);
     
@@ -20,6 +27,7 @@ class CreditCard{
         std::string name;
         int limit;
         double balance;
+        time_t dueDate;
 };
 
 // Print card information. 
