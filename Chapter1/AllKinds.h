e6b168832425d0280af2e6d050786c3a8ba41828
lt47 <layetee@gmail.com>
2024-09-30 18:11:33 -0500

    partly done reinforcement 17

---
+++ Chapter1/AllKinds.h
@@ -0,0 +1,29 @@
+#ifndef ALL_KINDS_H
+#define ALL_KINDS_H
+
+#include <string>
+#include <iostream>
+
+//Reinforcement 17. 
+class AllKinds{
+    public:
+        AllKinds(int intValue=1, long longValue=1, float floatValue=1.0);
+        int getIntValue() {return intValue;}
+        void setIntValue(int intVal){intValue = intVal;}
+        long getLongValue() {return longValue;}
+        void setLongValue(long longVal){longValue = longVal;}
+        float getFloatValue() {return floatValue;}
+        void setFloatValue(float floatVal){floatValue = floatVal;}
+
+        float sumOfEachCombination();
+    
+    private: 
+        int intValue;
+        long longValue;
+        float floatValue;
+};
+
+
+// Print All Kinds information. 
+std::ostream& operator<<(std::ostream& out, const AllKinds& ak);
+#endif
