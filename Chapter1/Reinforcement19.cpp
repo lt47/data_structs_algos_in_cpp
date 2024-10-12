92e209dd58c4e1bf3c0ec7e1f4d29655218c3650
lt47 <layetee@gmail.com>
2024-10-12 10:57:57 -0500

    completed reinforcement 19.

--- Chapter1/Reinforcement19.cpp
+++ Chapter1/Reinforcement19.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <bitset>
 
 /*Want to get into the habit of using unsigned int when appropriate because -
 "Unsigned integers represent only non-negative integers and have a larger 
@@ -7,5 +6,16 @@ range of positive numbers than signed integers because the number of bits is the
 bool isTwoPower(unsigned int i){
     /* An easy way to tell if an int is a power of 2 using binary, 
     is to see if there is only one 1 in the binary representation followed by 0s. */
-    std::string binary = std::bitset<8>(i).to_string(); //to binary
+    /* Next we can use the bitwise operator '&' to compare both i and i -1. If they share the same bit number of 1 and placement 
+    at any point, it will return 1 and it will return 0 otherwise. That is, if both the bits are 1, then it results is also 1; otherwise, the result is 0. Since ints with a power of 2 only have one 1 and there is an additional digit in power of 2 
+    binary representations that the previous int does not contain, it is a safe assumption. E.g. 8 is represented by 00001000 and 7 is represented by 00000111. 
+    Assuming an 8 bit size is used to hold the binary numbers. */
+    if((i & (i - 1)) == 0){
+        return true;
+    }
+    else return false;
+}
+
+int main(){
+    std::cout << isTwoPower(4) << std::endl;
 }
